e2b9161d220afa8b9f31b2b7b93821d4baa4b79c
alok27a <alok27a@gmail.com>
2022-09-04 18:37:39 +0530

    Dark mode added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
+import { DarkMode } from '@chakra-ui/react';
 import './App.css';
 import Home from './components/Home';
 
 function App() {
   return (
     <>
+    <DarkMode>
       <Home/>
+    </DarkMode>
     </>
   );
 }
